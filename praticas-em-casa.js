55ae77b6b3f71d2ffcf45975d4a31bd13958c814
lilianesantosdev''git config --global user.namegit config --global user.name lilianesantosdev <lilianesantospp@gmail.com>
2020-08-28 23:53:40 -0300

    exercicios feitos

---
+++ praticas-em-casa.js
@@ -0,0 +1,40 @@
+function nomeMesExtenso (numeroMes) {
+if (numeroMes === 1) {
+    return "janeiro"
+}  
+if (numeroMes === 2) {
+    return "fevereiro"
+}  
+if (numeroMes === 3) {
+    return "março"
+}  
+if (numeroMes === 4) {
+    return "abril"
+}  
+if (numeroMes === 5) {
+    return "maio"
+}  
+if (numeroMes === 6) {
+    return "junho"
+}  
+if (numeroMes === 7) {
+    return "julho"
+}  
+if (numeroMes === 8) {
+    return "agosto"
+}  
+if (numeroMes === 9) {
+    return "setembro"
+}  
+if (numeroMes === 10) {
+    return "outubro"
+}  
+if (numeroMes === 11) {
+    return "novembro"
+}  
+if (numeroMes === 12) {
+    return "dezembro"
+}  
+}
+
+console.log (nomeMesExtenso (12))
